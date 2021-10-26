0e2fae18a85fc64624079175b5cb3586bcf448a6
Chenying Lyu <ellen_lyu@163.com>
2021-10-27 01:41:14 +0800

    Get device's location via Google Map API, and update the longitude and latitude to the form.

--- app/src/main/java/com/comp90018/proj2/ui/sendPost/SendPostActivity.java
+++ app/src/main/java/com/comp90018/proj2/ui/sendPost/SendPostActivity.java
@@ -1,17 +1,25 @@
 package com.comp90018.proj2.ui.sendPost;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
-
-import com.comp90018.proj2.databinding.ActivityLoginBinding;
 import com.comp90018.proj2.databinding.ActivitySendPostBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SendPostActivity extends AppCompatActivity {
@@ -20,6 +28,18 @@ public class SendPostActivity extends AppCompatActivity {
     private String TAG = "SendPostActivity";
     private SendPostViewModel sendPostViewModel;
     private ActivitySendPostBinding binding;
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Location lastKnownLocation;
+    private boolean locationPermissionGranted = false;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     /** Auth */
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -39,9 +59,81 @@ public class SendPostActivity extends AppCompatActivity {
         sendPostViewModel = new ViewModelProvider(this, new SendPostViewModelFactory(getApplicationContext()))
                 .get(SendPostViewModel.class);
 
+        // Get user's location
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLocationPermission();
+        getDeviceLocation();
+
         Log.i(TAG, "onCreate: ");
 
+        // Latitude: add observer
+        final EditText textPostLat = binding.textPostLat;
+        sendPostViewModel.getLatitude().observe(this, textPostLat::setText);
+
+        // Longitude: add observer
+        final EditText textPostLon = binding.textPostLon;
+        sendPostViewModel.getLongitude().observe(this, textPostLon::setText);
     }
+
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+
+    // [START maps_current_place_location_permission]
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+    // [END maps_current_place_location_permission]
+
+
+    // [START maps_current_place_get_device_location]
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                Log.i(TAG, "Your Location: " + "\n" + "Latitude: " +
+                                        lastKnownLocation.getLatitude() + "\n" + "Longitude: " + lastKnownLocation.getLongitude());
+                                sendPostViewModel.updateLocation(lastKnownLocation.getLongitude(), lastKnownLocation.getLatitude());
+                            } else {
+                                Log.d(TAG, "lastKnownLocation: " + lastKnownLocation);
+                            }
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+    // [END maps_current_place_get_device_location]
 
     @Override
     protected void onDestroy() {
--- app/src/main/java/com/comp90018/proj2/ui/sendPost/SendPostViewModel.java
+++ app/src/main/java/com/comp90018/proj2/ui/sendPost/SendPostViewModel.java
@@ -1,22 +1,38 @@
 package com.comp90018.proj2.ui.sendPost;
 
-import androidx.lifecycle.LiveData;
+import android.location.Location;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.comp90018.proj2.R;
-import com.comp90018.proj2.ui.login.LoginFormState;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class SendPostViewModel extends ViewModel {
     private String TAG = "SendPostViewModel";
 
-    private MutableLiveData<LoginFormState> loginFormState = new MutableLiveData<>();
+    private MutableLiveData<String> longitude;
+    private MutableLiveData<String> latitude;
 
-    SendPostViewModel() { }
 
-    public void updateLocation(String longitude, String latitude) {
-
+    SendPostViewModel() {
+        longitude = new MutableLiveData<String>();
+        latitude = new MutableLiveData<String>();
     }
 
+    public MutableLiveData<String> getLongitude() {
+        return longitude;
+    }
+
+    public MutableLiveData<String> getLatitude() {
+        return latitude;
+    }
+
+    public void updateLocation(double longitude, double latitude) {
+        this.longitude.setValue(String.valueOf(longitude));
+        this.latitude.setValue(String.valueOf(latitude));
+    }
 
 }
