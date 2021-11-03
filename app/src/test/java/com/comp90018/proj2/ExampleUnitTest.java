4e3b0c460645359618f5bf76fa301b8891a4e0a5
Chenying Lyu <ellen_lyu@163.com>
2021-11-03 20:53:03 +0800

    fix error login message. add test to send post.

--- app/src/test/java/com/comp90018/proj2/ExampleUnitTest.java
+++ app/src/test/java/com/comp90018/proj2/ExampleUnitTest.java
@@ -5,28 +5,18 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 
@@ -37,74 +27,8 @@ import static org.junit.Assert.assertEquals;
  */
 public class ExampleUnitTest {
 
-    /** Firebase **/
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-    @BeforeClass
-    public void login() {
-        mAuth.signInWithEmailAndPassword("ccccc@gmail.com", "cw980629lm");
-    }
-
-
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    public void fireStore_send() {
-        System.out.println(mAuth.getCurrentUser().getUid());
-        assertEquals(4, 2 + 2);
-
-        // Test
-//        Map<String, Object> user = new HashMap<>();
-//        user.put("PostImage", "PostImage");
-//        user.put("PostLocation", "PostLocation");
-//        user.put("PostSpecies", "Cat");
-//        user.put("PostTime", LocalDateTime.now());
-//        user.put("PostType", "Animal");
-//        user.put("UserId", mAuth.getUid());
-//
-//        db.collection("Post")
-//                .add(user)
-//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                    @Override
-//                    public void onSuccess(DocumentReference documentReference) {
-////                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-////                        Log.w(TAG, "Error adding document", e);
-//                    }
-//                });
-    }
-
-    @Test
-    public void fireStore_get() {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-        // Create a reference to the cities collection
-        CollectionReference postsRef = db.collection("Post");
-
-        // Create a query against the collection.
-        Query query = postsRef.whereEqualTo("PostSpecies", "Cat");
-        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    QuerySnapshot documents = task.getResult();
-                    if (!documents.isEmpty()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            Log.d("Test", "DocumentSnapshot data: " + document.getData());
-                        }
-                    }
-                }
-            }
-        });
-
     }
 }
